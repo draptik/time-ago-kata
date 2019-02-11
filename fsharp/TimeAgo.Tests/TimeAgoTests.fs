bb154c43b949816656d5bf6e8eeb3de544cc59a3
Patrick Drechsler <patrick@pdrechsler.de>
2019-02-11 22:52:39 +0100

    first fsharp test

--- fsharp/TimeAgo.Tests/TimeAgoTests.fs
+++ fsharp/TimeAgo.Tests/TimeAgoTests.fs
@@ -1,3 +1,11 @@
 module TimeAgoTests
 
+open FsUnit.Xunit
+open Xunit
 
+let timeAgo secondsToSubract =
+    "vor 10 Sekunden"
+
+[<Fact>]
+let ``returns 10 seconds ago`` () =
+    timeAgo 10 |> should equal "vor 10 Sekunden"
